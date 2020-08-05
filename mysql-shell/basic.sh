c19796242d2738069745757d086b66f36d28f49c
gofird <2089359811@qq.com>
2020-08-05 13:42:34 +0800

    basic comp

--- mysql-shell/basic.sh
+++ mysql-shell/basic.sh
@@ -3,7 +3,7 @@
 #
 #
 ###################### basic env 
-mkdir -p  /app/mysql /data/330{6,7,8}
+mkdir -p  /app/mysql /data/330{6,7,8}/data
 tar xvf mysql-5.7.30-linux-glibc2.12-x86_64.tar.gz -C /app/
 ln -s mysql-5.7.30-linux-glibc2.12-x86_64/ mysql
 
@@ -22,6 +22,6 @@ source /etc/profile
 
 ##################### data init 数据库安全初始化 
 #注意mysql密码
-mysqld --initialize  --user=mysql --basedir=/app/mysql --datadir=/data/3306
-# mysqld --initialize  --user=mysql --basedir=/app/mysql --datadir=/data/3307
-# mysqld --initialize  --user=mysql --basedir=/app/mysql --datadir=/data/3308
+mysqld --initialize  --user=mysql --basedir=/app/mysql --datadir=/data/3306/data
+# mysqld --initialize  --user=mysql --basedir=/app/mysql --datadir=/data/3307/data
+# mysqld --initialize  --user=mysql --basedir=/app/mysql --datadir=/data/3308/data
